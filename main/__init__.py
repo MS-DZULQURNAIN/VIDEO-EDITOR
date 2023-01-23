384657b99930e2327360ed1ef1a7779218d44727
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-23 13:10:21 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,12 +7,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
-AUTH_USERS = config("AUTH_USERS", default=None, cast=int)
-LOG_CHANNEL = config("LOG_CHANNEL", default=None)
+API_ID = config("26915599", default=None, cast=int)
+API_HASH = config("76438a4fa64f9e43b3921a204a79e000", default=None)
+BOT_TOKEN = config("5897361812:AAEMTxJoGlGudtfchrL2mSNSmv7Y4ODu3kw", default=None)
+BOT_UN = config("MsVideoEDITOR_bot", default=None)
+AUTH_USERS = config("5463819478", default=None, cast=int)
+LOG_CHANNEL = config("mylogpubmsdeploy", default=None)
 LOG_ID = config("LOG_ID", default=None)
 FORCESUB = config("FORCESUB", default=None)
 FORCESUB_UN = config("FORCESUB_UN", default=None)
