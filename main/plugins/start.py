0b0e1edb1bd611194bc40837c629da948c2db1c5
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-26 00:14:25 +0700

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -29,7 +29,7 @@ async def start(event):
                                 Button.url("DEVELOPER👤", url="https://t.me/ms_dzulqurnain")
                               ]) 
     tag = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
-    await Drone.send_message(int(ACCESS_CHANNEL), f'@{tag}{id} Memulai bot')
+    await Drone.send_message(int(ACCESS_CHANNEL), f'@{tag} Memulai bot\n\n id : `{sender_id}`')
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/help"))
 async def help(event):
