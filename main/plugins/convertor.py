7702c04463639de0eebd24da0b045e8c8f87cdc9
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-25 22:21:39 +0700

    Update convertor.py

--- main/plugins/convertor.py
+++ main/plugins/convertor.py
@@ -27,7 +27,7 @@ from LOCAL.localisation import SUPPORT_LINK, JPG, JPG2
 
 async def mp3(event, msg):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Sedang mengconvert...", reply_to=msg.id)
     if hasattr(msg.media, "document"):
         file = msg.media.document
     else:
@@ -50,18 +50,18 @@ async def mp3(event, msg):
         out = dt.now().isoformat("_", "seconds")
     try:
         DT = time.time()
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        await fast_download(name, file, Drone, edit, DT, "**MENDOWNLOAD:**")
     except Exception as e:
         os.rmdir("audioconvert")
         print(e)
-        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mendownload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         await edit.edit("Converting.")
         bash(f"ffmpeg -i {name} -codec:a libmp3lame -q:a 0 {out}.mp3")
     except Exception as e:
         os.rmdir("audioconvert")
         print(e)
-        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengconvert!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         UT = time.time()
         uploader = await fast_upload(f'{out}.mp3', f'{out}.mp3', UT, Drone, edit, '**UPLOADING:**')
@@ -69,14 +69,14 @@ async def mp3(event, msg):
     except Exception as e:
         os.rmdir("audioconvert")
         print(e)
-        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengupload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     await edit.delete()
     os.remove(name)
     os.remove(f'{out}.mp3')                           
                        
 async def flac(event, msg):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Sedang mengconvert...", reply_to=msg.id)
     if hasattr(msg.media, "document"):
         file = msg.media.document
     else:
@@ -99,11 +99,11 @@ async def flac(event, msg):
         out = dt.now().isoformat("_", "seconds")
     try:
         DT = time.time()
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        await fast_download(name, file, Drone, edit, DT, "**MENDOWNLOAD:**")
     except Exception as e:
         os.rmdir("audioconvert")
         print(e)
-        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mendownload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         await edit.edit("Converting.")
         bash(f"ffmpeg -i {name} -codec:a libmp3lame -q:a 0 {out}.mp3")
@@ -111,7 +111,7 @@ async def flac(event, msg):
     except Exception as e:
         os.rmdir("audioconvert")
         print(e)
-        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengconvert!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         UT = time.time()
         uploader = await fast_upload(f'{out}.flac', f'{out}.flac', UT, Drone, edit, '**UPLOADING:**')
@@ -119,7 +119,7 @@ async def flac(event, msg):
     except Exception as e:
         os.rmdir("audioconvert")
         print(e)
-        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengupload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     await edit.delete()
     os.remove(name)
     os.remove(f'{out}.mp3')                           
@@ -127,7 +127,7 @@ async def flac(event, msg):
 
 async def wav(event, msg):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Sedang mengconvert...", reply_to=msg.id)
     if hasattr(msg.media, "document"):
         file = msg.media.document
     else:
@@ -150,11 +150,11 @@ async def wav(event, msg):
         out = dt.now().isoformat("_", "seconds")
     try:
         DT = time.time()
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        await fast_download(name, file, Drone, edit, DT, "**MENDOWNLOAD:**")
     except Exception as e:
         os.rmdir("audioconvert")
         print(e)
-        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mendownload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         await edit.edit("Converting.")
         bash(f"ffmpeg -i {name} -codec:a libmp3lame -q:a 0 {out}.mp3")
@@ -162,7 +162,7 @@ async def wav(event, msg):
     except Exception as e:
         os.rmdir("audioconvert")
         print(e)
-        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengconvert!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         UT = time.time()
         uploader = await fast_upload(f'{out}.wav', f'{out}.wav', UT, Drone, edit, '**UPLOADING:**')
@@ -170,7 +170,7 @@ async def wav(event, msg):
     except Exception as e:
         os.rmdir("audioconvert")
         print(e)
-        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengupload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     await edit.delete()
     os.remove(name)
     os.remove(f'{out}.mp3')                           
@@ -178,7 +178,7 @@ async def wav(event, msg):
                                        
 async def mp4(event, msg):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Sedang mengconvert...", reply_to=msg.id)
     if hasattr(msg.media, "document"):
         file = msg.media.document
     else:
@@ -201,29 +201,29 @@ async def mp4(event, msg):
         out = dt.now().isoformat("_", "seconds")
     try:
         DT = time.time()
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        await fast_download(name, file, Drone, edit, DT, "**MENDOWNLOAD:**")
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mendownload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         await edit.edit("Converting.")
         rename(name, f'{out}.mp4')
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengconvert!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         UT = time.time()
         uploader = await fast_upload(f'{out}.mp4', f'{out}.mp4', UT, Drone, edit, '**UPLOADING:**')
         await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**CONVERTED by** : @{BOT_UN}', force_document=True)
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengupload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     await edit.delete()                      
     os.remove(f'{out}.mp4')                 
                                            
 async def mkv(event, msg):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Sedang mengconvert...", reply_to=msg.id)
     if hasattr(msg.media, "document"):
         file = msg.media.document
     else:
@@ -246,29 +246,29 @@ async def mkv(event, msg):
         out = dt.now().isoformat("_", "seconds") + ".mkv"
     try:
         DT = time.time()
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        await fast_download(name, file, Drone, edit, DT, "**MENDOWNLOAD:**")
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mendownload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         await edit.edit("Converting.")
         rename(name, f'{out}')
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengconvert!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         UT = time.time()
-        uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**CONVERTED by** : @{BOT_UN}', force_document=True)
+        uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**MENGUPLOAD:**')
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'TELAH TERCONVERT dari: @{BOT_UN}\n\n Owner : @ms_dzulqurnain👤', force_document=True)
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengupload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     await edit.delete()                        
     os.remove(f'{out}')
              
 async def webm(event, msg):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Sedang mengconvert...", reply_to=msg.id)
     if hasattr(msg.media, "document"):
         file = msg.media.document
     else:
@@ -291,29 +291,29 @@ async def webm(event, msg):
         out = dt.now().isoformat("_", "seconds") + ".webm"
     try:
         DT = time.time()
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        await fast_download(name, file, Drone, edit, DT, "**MENDOWNLOAD:**")
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mendownload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         await edit.edit("Converting.")
         rename(name, f'{out}')
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengconvert!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         UT = time.time()
-        uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**CONVERTED by** : @{BOT_UN}', force_document=True)
+        uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**MENGUPLOAD:**')
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'TELAH TERCONVERT dari: @{BOT_UN}\n\n Owner : @ms_dzulqurnain👤', force_document=True)
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengupload\n\nContact [SUPPORT]({SUPPORT_LINK})")
     await edit.delete()                 
     os.remove(f'{out}')
              
 async def file(event, msg):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Sedang mengconvert...", reply_to=msg.id)
     if hasattr(msg.media, "document"):
         file = msg.media.document
     else:
@@ -332,23 +332,23 @@ async def file(event, msg):
         name = "media_" + dt.now().isoformat("_", "seconds") + ".webm"      
     try:
         DT = time.time()
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        await fast_download(name, file, Drone, edit, DT, "**MENDOWNLOAD:**")
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mendownload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         UT = time.time()
-        uploader = await fast_upload(f'{name}', f'{name}', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'**CONVERTED by** : @{BOT_UN}', force_document=True)
+        uploader = await fast_upload(f'{name}', f'{name}', UT, Drone, edit, '**MENGUPLOAD:**')
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG, caption=f'TELAH TERCONVERT dari: @{BOT_UN}\n\n Owner : @ms_dzulqurnain👤', force_document=True)
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengupload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     await edit.delete()
     os.remove(name)                           
     
 async def video(event, msg):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process!", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Sedang mengconvert...", reply_to=msg.id)
     if hasattr(msg.media, "document"):
         file = msg.media.document
     else:
@@ -371,16 +371,16 @@ async def video(event, msg):
         out = dt.now().isoformat("_", "seconds") + '.mp4'
     try:
         DT = time.time()
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        await fast_download(name, file, Drone, edit, DT, "**MENDOWNLOAD:**")
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while downloading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mendownload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         await edit.edit("Converting.")
         rename(name, f'{out}')
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while converting!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengconvert!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     try:
         metadata = video_metadata(out)
         width = metadata["width"]
@@ -388,11 +388,11 @@ async def video(event, msg):
         duration = metadata["duration"]
         attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]           
         UT = time.time()
-        uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**UPLOADING:**')
-        await Drone.send_file(event.chat_id, uploader, thumb=JPG2, caption=f'**CONVERTED by** : @{BOT_UN}', attributes=attributes, force_document=False)
+        uploader = await fast_upload(f'{out}', f'{out}', UT, Drone, edit, '**MENGUPLOAD:**')
+        await Drone.send_file(event.chat_id, uploader, thumb=JPG2, caption=f'*TELAH TERCONVERT dari: @{BOT_UN}\n\n Owner : @ms_dzulqurnain👤', attributes=attributes, force_document=False)
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while uploading!\n\nContact [SUPPORT]({SUPPORT_LINK})")
+        return await edit.edit(f"Terjadi kesalahan saat mengupload!\n\nContact [SUPPORT]({SUPPORT_LINK})")
     await edit.delete()
     os.remove(out)                           
     
