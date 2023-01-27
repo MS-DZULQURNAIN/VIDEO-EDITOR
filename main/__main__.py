5776396ae3480da0690277e0638d7cbd09df9a8e
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-27 10:50:34 +0700

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -2,7 +2,7 @@ import glob
 from pathlib import Path
 from main.utils import load_plugins
 import logging
-from . import Drone
+from . import MSDZULQURNAIN
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
@@ -19,4 +19,4 @@ print("Bot telah aktif🔥🔥🔥")
 print("@MSDEPLOY | @MS_DZULQURNAIN_NET")
 
 if __name__ == "__main__":
-    Drone.run_until_disconnected()
+    MSDZULQURNAIN.run_until_disconnected()
