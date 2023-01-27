1b2a7ebeba2cab80681198324e7066e9998b5ec5
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-01-27 15:14:03 +0700

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -60,7 +60,7 @@ async def join(event):
                                 Button.url("CHANNEL 1", url="https://t.me/MSDEPLOY")],
                                 [
                                 Button.url("CHANNEL 2", url="https://t.me/MS_DZULQURNAIN_NET")],
-                                [Button.url("CHANNEL 3", url="https://t.me/ms_dzulqurnain")],
+                                [Button.url("CHANNEL 3", url="https://semawur.com/AjAHI")],
                                 [Button.inline("TUTUP💝", data="tutup")
                               ]]) 
  #-----------------------------------------------------------------------------------------------                            
